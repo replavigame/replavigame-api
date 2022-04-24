4fa3e2944395912f95f233128594fabd3168735d
LucianoLopez <lucianolopez4000@hotmail.com>
2022-04-24 16:02:50 -0500

    Start project

---
+++ replavigame/src/test/java/com/replavigame/replavigame/ReplavigameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.replavigame.replavigame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplavigameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
