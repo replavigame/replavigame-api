af8434d99e52095b6be89020185463595d1d7c94
CarlosIzarra09 <eduizarra2@hotmail.com>
2022-05-31 18:18:26 -0500

    fix: Correct eureka lines in config coaching .yml

--- coaching-session-service/src/main/java/com/coachingsession/CoachingSessionServiceApplication.java
+++ coaching-session-service/src/main/java/com/coachingsession/CoachingSessionServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
-@EnableEurekaClient
 @SpringBootApplication
+@EnableEurekaClient
 public class CoachingSessionServiceApplication {
 
 	public static void main(String[] args) {
