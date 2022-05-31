9b72f42c697902e26c956eead09d1a01cd99616b
CarlosIzarra09 <eduizarra2@hotmail.com>
2022-05-31 18:47:55 -0500

    feat: Add coach report template

---
+++ coach-report-service/coach-report-service/src/main/java/com/coachreportservice/CoachReportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coachreportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachReportServiceApplication.class, args);
+	}
+
+}
---
+++ coach-report-service/coach-report-service/src/test/java/com/coachreportservice/CoachReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coachreportservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachReportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
