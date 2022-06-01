a1261803ccadc54428276b2af0774b9825ae894e
CarlosIzarra09 <eduizarra2@hotmail.com>
2022-06-01 00:35:27 -0500

    feat: Add coachdocument and coachreport repositories and services interfaces

---
+++ coach-report-service/src/main/java/com/coachreport/coach_report/repository/CoachDocumentRepository.java
@@ -0,0 +1,12 @@
+package com.coachreport.coach_report.repository;
+
+import com.coachreport.coach_report.entity.CoachDocument;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CoachDocumentRepository extends JpaRepository<CoachDocument,Long> {
+    Optional<CoachDocument> getCoachDocumentById(Long id);
+    List<CoachDocument> findAllByCoachReportId(Long coachReportId);
+}
