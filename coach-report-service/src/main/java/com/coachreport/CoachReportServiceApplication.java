03045f49e8645dab5b79abaf8b9a529f389dc64e
CarlosIzarra09 <eduizarra2@hotmail.com>
2022-05-31 19:52:53 -0500

    chore: change jvm gradle to java 17.0.2

--- coach-report-service/src/main/java/com/coachreport/CoachReportServiceApplication.java
+++ coach-report-service/src/main/java/com/coachreport/CoachReportServiceApplication.java
@@ -1,9 +1,13 @@
-package com.coachreportservice;
+package com.coachreport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
+@EnableEurekaClient
 public class CoachReportServiceApplication {
 
 	public static void main(String[] args) {
--- coach-report-service/src/test/java/com/coachreport/coachreportservice/CoachReportServiceApplicationTests.java
+++ coach-report-service/src/test/java/com/coachreport/coachreportservice/CoachReportServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.coachreportservice;
+package com.coachreport.coachreportservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
