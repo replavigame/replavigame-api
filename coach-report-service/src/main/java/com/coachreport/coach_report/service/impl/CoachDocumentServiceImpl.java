592b48ae1e6553106b09f9f4730fca0e6c34d84e
CarlosIzarra09 <eduizarra2@hotmail.com>
2022-06-01 00:36:14 -0500

    feat: Add coachdocument and coachreport implemented services

---
+++ coach-report-service/src/main/java/com/coachreport/coach_report/service/impl/CoachDocumentServiceImpl.java
@@ -0,0 +1,113 @@
+package com.coachreport.coach_report.service.impl;
+
+import com.coachreport.coach_report.dto.CoachDocumentRequest;
+import com.coachreport.coach_report.dto.CoachDocumentResponse;
+import com.coachreport.coach_report.entity.CoachDocument;
+import com.coachreport.coach_report.repository.CoachDocumentRepository;
+import com.coachreport.coach_report.repository.CoachReportRepository;
+import com.coachreport.coach_report.service.CoachDocumentService;
+import com.coachreport.exception.ResourceNotFoundExceptionRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CoachDocumentServiceImpl implements CoachDocumentService {
+
+    @Autowired
+    private CoachDocumentRepository coachDocumentRepository;
+
+    @Autowired
+    private CoachReportRepository coachReportRepository;
+
+    private CoachDocumentResponse convertToResponse(CoachDocument entity) {
+        CoachDocumentResponse response = new CoachDocumentResponse();
+        response.setId(entity.getId());
+        response.setDocumentUrl(entity.getDocumentUrl());
+        response.setTitle(entity.getTitle());
+        response.setComment(entity.getComment());
+        response.setCoachreport_id(entity.getCoachReport().getId());
+        return response;
+    }
+
+    private CoachDocument convertToEntity(CoachDocumentRequest request) {
+        CoachDocument coachDocument = new CoachDocument();
+        coachDocument.setDocumentUrl(request.getDocumentUrl());
+        coachDocument.setTitle(request.getTitle());
+        coachDocument.setComment(request.getComment());
+        return coachDocument;
+    }
+
+
+    @Override
+    public CoachDocumentResponse getById(Long id) {
+        var entity = coachDocumentRepository.getCoachDocumentById(id)
+                .orElseThrow(() -> new ResourceNotFoundExceptionRequest("CoachDocument not found"));
+        return convertToResponse(entity);
+    }
+
+    @Override
+    public List<CoachDocumentResponse> getAll() {
+        var entities = coachDocumentRepository.findAll();
+        return entities.stream().map(this::convertToResponse)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<CoachDocumentResponse> getAllCoachDocumentsByCoachReportId(Long coachReportId) {
+        var entities = coachDocumentRepository.findAllByCoachReportId(coachReportId);
+        return entities.stream().map(this::convertToResponse)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CoachDocumentResponse createCoachDocumentByCoachReportId(CoachDocumentRequest coachDocumentRequest, Long coachReportId) {
+        var coachReport = coachReportRepository.getCoachReportById(coachReportId)
+                .orElseThrow(() -> new ResourceNotFoundExceptionRequest("CoachReport not found"));
+
+
+        var entity = convertToEntity(coachDocumentRequest);
+        entity.setCoachReport(coachReport);
+
+        try {
+            coachDocumentRepository.save(entity);
+            return convertToResponse(entity);
+        } catch (Exception e) {
+            throw new ResourceNotFoundExceptionRequest("Error occurred while creating CoachDocument");
+        }
+
+    }
+
+    @Override
+    public CoachDocumentResponse updateCoachDocumentByCoachReportId(CoachDocumentRequest coachDocumentRequest, Long coachReportId, Long id) {
+        var coachReport = coachReportRepository.getCoachReportById(coachReportId)
+                .orElseThrow(() -> new ResourceNotFoundExceptionRequest("CoachReport not found"));
+        var coachDocument = coachDocumentRepository.getCoachDocumentById(id)
+                .orElseThrow(() -> new ResourceNotFoundExceptionRequest("CoachDocument not found"));
+
+        coachDocument = convertToEntity(coachDocumentRequest);
+        coachDocument.setId(id);
+        coachDocument.setCoachReport(coachReport);
+
+        try {
+            coachDocumentRepository.save(coachDocument);
+            return convertToResponse(coachDocument);
+        } catch (Exception e) {
+            throw new ResourceNotFoundExceptionRequest("Error occurred while updating CoachDocument");
+        }
+    }
+
+    @Override
+    public void deleteCoachDocument(Long coachReportId, Long id) {
+        var coachReport = coachReportRepository.getCoachReportById(coachReportId)
+                .orElseThrow(() -> new ResourceNotFoundExceptionRequest("CoachReport not found"));
+        var coachDocument = coachDocumentRepository.getCoachDocumentById(id)
+                .orElseThrow(() -> new ResourceNotFoundExceptionRequest("CoachDocument not found"));
+
+        try {
+            coachDocumentRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new ResourceNotFoundExceptionRequest("Error occurred while deleting CoachDocument");
+        }
+    }
+}
---
+++ coach-report-service/src/main/java/com/coachreport/coach_report/service/impl/CoachReportServiceImpl.java
@@ -0,0 +1,98 @@
+package com.coachreport.coach_report.service.impl;
+
+import com.coachreport.coach_report.dto.CoachDocumentResponse;
+import com.coachreport.coach_report.dto.CoachReportRequest;
+import com.coachreport.coach_report.dto.CoachReportResponse;
+import com.coachreport.coach_report.entity.CoachReport;
+import com.coachreport.coach_report.repository.CoachReportRepository;
+import com.coachreport.coach_report.service.CoachReportService;
+import com.coachreport.exception.ResourceNotFoundExceptionRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CoachReportServiceImpl implements CoachReportService {
+
+    @Autowired
+    private CoachReportRepository coachReportRepository;
+
+    private CoachReportResponse convertToResponse(CoachReport entity) {
+        CoachReportResponse response = new CoachReportResponse();
+        response.setId(entity.getId());
+        response.setApproved(entity.getApproved());
+        response.setObservation(entity.getObservation());
+        response.setQualifiedDate(entity.getQualifiedDate());
+        response.setReceivedDate(entity.getQualifiedDate());
+        return response;
+    }
+
+    private CoachReport convertToEntity(CoachReportRequest request) {
+        CoachReport coachReport = new CoachReport();
+        coachReport.setApproved(request.getApproved());
+        coachReport.setObservation(request.getObservation());
+        coachReport.setQualifiedDate(request.getQualifiedDate());
+        coachReport.setReceivedDate(request.getReceivedDate());
+        return coachReport;
+    }
+
+
+    @Override
+    public List<CoachReportResponse> getAll() {
+        var entities = coachReportRepository.findAll();
+        return entities.stream().map(this::convertToResponse)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<CoachReportResponse> getAllCoachReportsByRangeOfReceivedDate() {
+        return null;
+    }
+
+    @Override
+    public CoachReportResponse getById(Long id) {
+        var entity = coachReportRepository.getCoachReportById(id)
+                .orElseThrow(() -> new ResourceNotFoundExceptionRequest("CoachReport not found"));
+        return convertToResponse(entity);
+    }
+
+    @Override
+    public CoachReportResponse create(CoachReportRequest coachReportRequest) {
+        var entity = convertToEntity(coachReportRequest);
+
+        try {
+            coachReportRepository.save(entity);
+            return convertToResponse(entity);
+        } catch (Exception e) {
+            throw new ResourceNotFoundExceptionRequest("Error occurred while creating CoachReport");
+        }
+    }
+
+    @Override
+    public CoachReportResponse update(CoachReportRequest coachReportRequest, Long id) {
+        var entity = coachReportRepository.getCoachReportById(id)
+                .orElseThrow(() -> new ResourceNotFoundExceptionRequest("CoachReport not found"));
+
+        entity = convertToEntity(coachReportRequest);
+        entity.setId(id);
+
+        try {
+            coachReportRepository.save(entity);
+            return convertToResponse(entity);
+        } catch (Exception e) {
+            throw new ResourceNotFoundExceptionRequest("Error occurred while updating CoachReport");
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        var entity = coachReportRepository.getCoachReportById(id)
+                .orElseThrow(() -> new ResourceNotFoundExceptionRequest("CoachReport not found"));
+
+        try {
+            coachReportRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new ResourceNotFoundExceptionRequest("Error occurred while deleting CoachReport");
+        }
+    }
+}
