31b01f3712debb98543d441510a22b406f75c32b
CarlosIzarra09 <eduizarra2@hotmail.com>
2022-06-03 00:55:57 -0500

    feat: add coaching-session-service to develop

--- coaching-session-service/src/main/java/com/coachingsession/coaching_session/client/CoachClient.java
+++ coaching-session-service/src/main/java/com/coachingsession/coaching_session/client/CoachClient.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "coach-service",fallback = CoachHystrixFallbackFactory.class)
 public interface CoachClient {
 
-    @GetMapping("/coachs/{id}")
+    @GetMapping("/coaches/{id}")
     ResponseEntity<Coach> geById(@PathVariable("id") Long id);
 }
