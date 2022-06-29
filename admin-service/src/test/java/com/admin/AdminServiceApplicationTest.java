45049ab000f26315d315defdcb81b6ddaa654cd6
Luciano Mardonio Lopez Duran <lucianolopez4000@hotmail.com>
2022-06-29 03:14:00 -0500

    feat: add admin service

--- admin-service/src/test/java/com/admin/AdminServiceApplicationTest.java
+++ admin-service/src/test/java/com/admin/AdminServiceApplicationTest.java
@@ -1,10 +1,10 @@
-package com.gateway;
+package com.admin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayApplicationTests {
+class AdminServiceApplicationTest {
 
 	@Test
 	void contextLoads() {
