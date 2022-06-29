32c88ff87280160b755d95266454b989123aaa26
Luciano Mardonio Lopez Duran <lucianolopez4000@hotmail.com>
2022-06-28 19:32:53 -0500

    feat: Config gateway

--- gateway-service/src/main/java/com/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/com/gateway/GatewayApplication.java
@@ -3,15 +3,9 @@ package com.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEurekaClient
 @SpringBootApplication
-@EnableHystrix
-@EnableFeignClients
-@EnableHystrixDashboard
 public class GatewayApplication {
 
 	public static void main(String[] args) {
