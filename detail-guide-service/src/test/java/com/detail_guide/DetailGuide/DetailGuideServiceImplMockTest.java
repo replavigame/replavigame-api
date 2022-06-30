6d565da14938aaa60bd383c6f63dc3d8588c8a4a
Luciano Mardonio Lopez Duran <lucianolopez4000@hotmail.com>
2022-06-29 23:01:14 -0500

    feat: DetailTest of Andree

--- detail-guide-service/src/test/java/com/detail_guide/DetailGuide/DetailGuideServiceImplMockTest.java
+++ detail-guide-service/src/test/java/com/detail_guide/DetailGuide/DetailGuideServiceImplMockTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.Optional;
 
@@ -13,6 +14,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
